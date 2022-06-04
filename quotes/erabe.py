47e9d52af7dc227868e0081cab126374f3873146
sumaneko <oppeke10@hotmail.co.jp>
2022-06-05 01:33:16 +0900

    no message

--- quotes/erabe.py
+++ quotes/erabe.py
@@ -15,6 +15,38 @@ def get_erabe(bot, m, *args):
 	else:
 		#元気の時
 		choices = list(args)
+		quotes = [[]]
+
+		if len(choices) < 0:
+			return "えっ、選択肢が無いです・・・"
+
+		if len(choices) = 1:
+			return choices[0] + "しか選べないじゃないですか！"
+
+		for i in range(len(choices)):
+			qoutes.append([100, choices[i]])
+		return get_quotes(quotes)
+
+"""
+
+		text = vote_text_prefix + choices.pop(0)
+		if len(choices) <= 0:
+			#マルバツ
+			vote = await ctx.send(text)
+			for emoji in vote_emoji_on_no_choice:
+				await vote.add_reaction(emoji)
+		else:
+			for index in range(len(commands)):
+				if index < len(vote_emoji_on_some_choice):
+					text += "\n" + vote_emoji_on_some_choice[index] + ":" + commands[index]
+				else:
+					text += "\n" + vote_text_choice_overflow_message
+					break
+			vote = await ctx.send(text)
+			for index in range(len(commands)):
+				if index < len(vote_emoji_on_some_choice):
+					await vote.add_reaction(vote_emoji_on_some_choice[index])
 
 	return choices[0] + choices[1]
 	#return pre + get_quotes(quotes) + suf
+"""
