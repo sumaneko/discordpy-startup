5bb06d5c907cfa1562b1c0b0523793492a3ae89e
sumaneko <oppeke10@hotmail.co.jp>
2022-06-05 01:09:53 +0900

    no message

--- quotes/erabe.py
+++ quotes/erabe.py
@@ -1,20 +1,20 @@
 from mylib.mymodule import get_quotes
 from mymodule.ryonage_bot import RyonageBot
 def get_erabe(bot, m, *args):
-    pre = ""
-    suf = ""
-    name = m.author.name if m.author.nick is None else m.author.nick
+	pre = ""
+	suf = ""
+	name = m.author.name if m.author.nick is None else m.author.nick
 
-    if  bot.get_hp() <= bot.dying_hp:
-        #瀕死の時
-        quotes = [
-            [100 , "知りま、せん・・・そんなの・・・"],
-            [100 , f"私、を・・・虐める{name}さんに・・・ラッキーアイテムなん、て・・・無いです・・・"],
-            [100 , "私が欲しい、ですよ・・・そんなの・・・"]
-            ]
-    else:
-        #元気の時
-        choices = list(args)
+	if  bot.get_hp() <= bot.dying_hp:
+		#瀕死の時
+		quotes = [
+			[100 , "知りま、せん・・・そんなの・・・"],
+			[100 , f"私、を・・・虐める{name}さんに・・・ラッキーアイテムなん、て・・・無いです・・・"],
+			[100 , "私が欲しい、ですよ・・・そんなの・・・"]
+			]
+	else:
+		#元気の時
+		choices = list(args)
 
-     return choices[0] + choices[1]   
-    #return pre + get_quotes(quotes) + suf
+	return choices[0] + choices[1]
+	#return pre + get_quotes(quotes) + suf
