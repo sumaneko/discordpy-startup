e4c872e93d98a930743672e5bfcd65c2eca55728
sumaneko <oppeke10@hotmail.co.jp>
2022-06-05 02:22:24 +0900

    no message

--- quotes/erabe.py
+++ quotes/erabe.py
@@ -12,6 +12,7 @@ def get_erabe(bot, m, *args):
 			[100 , "勝手に・・・やっててくだ・・・さい・・・"],
 			[100 , "選べ・・・と言われ・・・ても・・・"]
 			]
+		return get_quotes(quotes)
 	else:
 		#元気の時
 		choices = list(args)
@@ -22,12 +23,29 @@ def get_erabe(bot, m, *args):
 		if len(choices) == 1:
 			return choices[0] + "しか選べないじゃないですか！"
 
+		#ここから本題
+		pre = [
+			[100, "そうですね・・・"],
+			[100, "私は"],
+			[100, "私なら"],
+			[100, "どうしても選べというなら・・・"],
+			[100, "え、うーん・・・"],
+			[100, "私だったら"]
+			]
+		suf = [
+			[100, "がいいと思います"],
+			[100, "にします"],
+			[100, "ですかね・・・"],
+			[100, "！"],
+			[100, "を選びます"]
+			]
 		#中身をquotesにつっこんでいく
 		quotes = list(list())
 		for choice in choices:
 			quotes.append([100, choice])
 
-	return get_quotes(quotes)
+		return get_quotes(pre) + get_quotes(quotes) + get_quotes(suf)
+	return
 
 """
 
