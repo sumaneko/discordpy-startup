2f762a9321deebcb2dd9d1a266cb632dbbd5b543
sumaneko <oppeke10@hotmail.co.jp>
2022-06-29 23:41:22 +0900

    no message

--- discordbot.py
+++ discordbot.py
@@ -200,8 +200,8 @@ async def erabe(ctx, *args):
 	await ctx.send(get_erabe(bot_chan, ctx.message, *args))
 
 @bot.command()
-async def dice(ctx, arg):
-	await ctx.send(get_dice(bot_chan, ctx.message, arg))
+async def dice(ctx, *arg):
+	await ctx.send(get_dice(bot_chan, ctx.message, *arg))
 
 
 #発言に反応する
