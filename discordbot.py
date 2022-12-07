8d2700aa3540d59c2083154c38ab2b547ca849e4
sumaneko <oppeke10@hotmail.co.jp>
2022-12-07 23:11:22 +0900

    no message

--- discordbot.py
+++ discordbot.py
@@ -221,7 +221,7 @@ async def dice(ctx, *arg):
 @bot.command()
 async def unchiku(ctx):
 	line = await ctx.send(get_unchiku(bot_chan, ctx.message))
-	await line.add_reaction("<:hee:1031502459652280320>")
+	await line.add_reaction("1031502459652280320")
 	#await line.add_reaction("<:sitteru:1031502489717059694>")
 	#await line.add_reaction("🇾")
 	#await line.add_reaction("🇳")
