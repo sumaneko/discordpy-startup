dd04b21b2436deb11140af39b2106b9fb9fbb54c
sumaneko <oppeke10@hotmail.co.jp>
2023-08-30 01:21:53 +0900

    no message

--- discordbot.py
+++ discordbot.py
@@ -114,7 +114,8 @@ async def sex(ctx):
 async def test(ctx):
 	#瀕死じゃないなら喋る
 	if bot_chan.dying_hp < bot_chan.get_hp():
-		await ctx.send("今はテストはしてないです")
+		await ctx.send("github actionsのテスト")
+		#await ctx.send("今はテストはしてないです")
 
 @bot.command()
 async def harapan(ctx):
