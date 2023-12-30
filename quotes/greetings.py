86a9837b7d7d8d7d4c006ff8980a56b1527937f6
sumaneko <oppeke10@hotmail.co.jp>
2023-12-30 23:26:54 +0900

    no message

--- quotes/greetings.py
+++ quotes/greetings.py
@@ -1,3 +1,4 @@
+import datetime
 from mylib.mymodule import get_quotes
 from mylib.mymodule import get_nick
 
@@ -5,6 +6,8 @@ from mylib.mymodule import get_nick
 def get_greetings(m):
 	t = ""
 	name = get_nick(m)
+	#JSTにするには普通に9時間ずらすだけ
+	dt_now = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=9)))
 	if "おやす" in m.content:
 		quotes = [
 			[100, "おやすみなさい～"],
@@ -54,11 +57,24 @@ def get_greetings(m):
 		return get_quotes(quotes)
 
 	if m.content == "あけおめ" or "あけましておめでとう" in m.content or "明けましておめでとう" in m.content:
-		quotes = [
-			[100, "あけおめですよ！"],
-			[100, "あけましておめでとうございます！"],
-			[100, "あけおめ～"]
-		]
-		return get_quotes(quotes)
+		if dt_now.month == 1:
+			quotes = [
+				[100, "あけおめですよ！"],
+				[100, "あけましておめでとうございます！"],
+				[100, "あけおめ～"]
+			]
+		elif dt_now.month == 12:
+			quotes = [
+				[100, "あけおめにはちょっと早いですよ？"],
+				[100, "それは来月でしょ！"],
+				[100, "まだあけてないです・・・"]
+			]
+		else:
+			quotes = [
+				[100, "はいはいあけおめあけおめ"],
+				[100, f"まだ{dt_now.month}月ですよ、からかってます？"],
+				[100, "あけませんよ！"]
+			]
+			return get_quotes(quotes)
 
 	return t
